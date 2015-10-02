45e5357104c775b1e9eca5bc3e214497dad45e25
hornT <neverov.nikolay@gmail.com>
2015-10-02 14:51:00 +0300

    string without split symbols

--- source/js/script.js
+++ source/js/script.js
@@ -22,23 +22,26 @@
 	25: parseDateTime,// date_time SIZE(12)
 	26: parseDate,// date SIZE(5)
 	27: parseTime,// time SIZE(4)
+	
+	126: fullPacket, // 7E
 };
 
 var index = 0;
-var bytes = [];
+var data_bytes = [];
 
+// Добавляет строку лога с данными разбора
 function addLog(element, dataIndex, size, text){
 
 	var node = document.createElement('div');
 	element.appendChild(node);
 	var start = index;
-	node.innerHTML = bytes.slice(index, index + size + dataIndex).join('-') + " - " + text;
-	node.onclick = function(){ selectPacket(start, size + dataIndex); };
+	node.innerHTML = data_bytes.slice(index, index + size + dataIndex).join('-') + " - " + text;
+	//node.onclick = function(){ selectPacket(start, size + dataIndex); };
 	
 	index += dataIndex + size;
 }
 
-// Выделение конкретного пакета
+// Выделение конкретного пакета по нажатию на лог
 function selectPacket(start, size){
 	
 	console.log('selectPacket. start: %s, size: %s', start, size);
@@ -47,19 +50,24 @@ function selectPacket(start, size){
 }
 
 function parse(){
-	var dataStr = document.querySelector('#dlmsPacket').value;
+	var dPacket = document.querySelector('#dlmsPacket');
+	var dataStr = dPacket.value;
 	index = 0;
 	//console.log(dataStr);
 	
-	dataStr = dataStr.replace(/\s+/g, ' ');
-	document.querySelector('#dlmsPacket').value = dataStr;
-	bytes = dataStr.split(/[-_ ]/);
-	console.log('bytes.length: %s', bytes.length);
+	dataStr = dataStr.replace(/[-_ ]/g, '');
+	//dPacket.value = dataStr;
+	//data_bytes = dataStr.split(/[-_ ]/);
+	data_bytes = dPacket.match(/.{2}/g);
+	dPacket.value = data_bytes.join('-');
+	console.log('data_bytes.length: %s', data_bytes.length);
 	
-	var type = parseInt(bytes[index], 16);
+	var type = parseInt(data_bytes[index], 16);
 	var fragment = document.createDocumentFragment();
+	
 	parseFuncs[type](fragment);
 	
+	// Очистка старого лога и добавление нового
 	var logNode = document.querySelector('#log');
 	while (logNode.firstChild) {
 		logNode.removeChild(logNode.firstChild);
@@ -67,18 +75,37 @@ function parse(){
 	logNode.appendChild(fragment);
 }
 
+// Разбор пакета целиком
+function fullPacket(element){
+	// Выделить из пакетов данные и разобрать
+	
+	// разобрать заголовок пакета
+	// 1 и последний элементы 7E
+	// 2 - Формат -- A0
+	// 3 длина
+	// 4+5 адрес получателя
+	// 6 адрес отправителя
+	// 7 = 93 - control field (SNRM - Set normal response mode)
+	// 8+9 CRC заголовка
+	addLog(element, 1, 4, 'Integer32 = ' + val);
+	
+	// разобрать данные
+	var type = parseInt(data_bytes[index], 16);
+	parseFuncs[type](fragment);
+}
+
 // Разбор массивов 01
 function parseArray(element){
 	var node = document.createElement('div');
 	element.appendChild(node);
 	
-	var size = parseInt(bytes[index + 1], 16);
-	node.innerHTML = bytes.slice(index, index + 2).join('-') + " - Массив размером " + size;
+	var size = parseInt(data_bytes[index + 1], 16);
+	node.innerHTML = data_bytes.slice(index, index + 2).join('-') + " - Массив размером " + size;
 	
 	index += 2;
 	
 	for(var i = 0; i < size; i++){
-		var type = parseInt(bytes[index], 16);
+		var type = parseInt(data_bytes[index], 16);
 		parseFuncs[type](node);
 	}
 }
@@ -88,13 +115,13 @@ function parseStruct(element){
 	var node = document.createElement('div');
 	element.appendChild(node);
 	
-	var size = parseInt(bytes[index + 1], 16);
-	node.innerHTML = bytes.slice(index, index + 2).join('-') + " - Структура размером " + size;
+	var size = parseInt(data_bytes[index + 1], 16);
+	node.innerHTML = data_bytes.slice(index, index + 2).join('-') + " - Структура размером " + size;
 	
 	index += 2;
 	
 	for(var i = 0; i < size; i++){
-		var type = parseInt(bytes[index], 16);
+		var type = parseInt(data_bytes[index], 16);
 		parseFuncs[type](node);
 	}
 }
@@ -102,97 +129,97 @@ function parseStruct(element){
 // Разбор Integer32 05 (5)
 function parseInteger32(element){
 	
-	var val = parseInt(bytes[index + 1], 16);
+	var val = parseInt(data_bytes[index + 1], 16);
 	addLog(element, 1, 4, 'Integer32 = ' + val);
 }
 
 // Разбор Unsigned32 06 (6)
 function parseUnsigned32(element){
 
-	var val = parseInt(bytes[index + 1], 16);
+	var val = parseInt(data_bytes[index + 1], 16);
 	addLog(element, 1, 4, 'Unsigned32 = ' + val);
 }
 
 // Разбор строки 09
 function parseString(element){
 
-	var size = parseInt(bytes[index + 1], 16);
+	var size = parseInt(data_bytes[index + 1], 16);
 	addLog(element, 2, size, 'Строка');
 }
 
 // Разбор Integer8 0F (15)
 function parseInteger8(element){
 
-	var val = parseInt(bytes[index + 1], 16);
+	var val = parseInt(data_bytes[index + 1], 16);
 	addLog(element, 1, 1, 'Integer8 = ' + val);
 }
 
 // Разбор Integer16 10 (16)
 function parseInteger16(element){
 
-	var val = parseInt(bytes[index + 1] + bytes[index + 2], 16);
+	var val = parseInt(data_bytes[index + 1] + data_bytes[index + 2], 16);
 	addLog(element, 1, 2, 'Integer16 = ' + val);
 }
 
 // Разбор Unsigned8 11 (17)
 function parseUnsigned8(element){
 	
-	var val = parseInt(bytes[index + 1], 16);
+	var val = parseInt(data_bytes[index + 1], 16);
 	addLog(element, 1, 1, 'Unsigned8 = ' + val);
 }
 
 // Разбор long_unsigned 12 (18)
 function parseUnsigned16(element){
 
-	var val = parseInt(bytes[index + 1] + bytes[index + 2], 16);
+	var val = parseInt(data_bytes[index + 1] + data_bytes[index + 2], 16);
 	addLog(element, 1, 2, 'Unsigned16 = ' + val);
 }
 
 // Разбор Integer64 14 (20)
 function parseInteger64(element){
 	
-	var val = 'hz'; //parseInt(bytes[index + 1] + bytes[index + 2], 16);
+	var val = 'hz'; //parseInt(data_bytes[index + 1] + data_bytes[index + 2], 16);
 	addLog(element, 1, 8, 'Integer64 = ' + val);
 }
 
 // Разбор Unsigned64 15 (21)
 function parseUnsigned64(element){
 
-	var val = 'hz'; //parseInt(bytes[index + 1] + bytes[index + 2], 16);
+	var val = 'hz'; //parseInt(data_bytes[index + 1] + data_bytes[index + 2], 16);
 	addLog(element, 1, 8, 'Unsigned64 = ' + val);
 }
 
 // Разбор float32 17 (23)
 function parseFloat32(element){
 
-	var val = 'hz'; //parseInt(bytes[index + 1] + bytes[index + 2], 16);
+	var val = 'hz'; //parseInt(data_bytes[index + 1] + data_bytes[index + 2], 16);
 	addLog(element, 1, 4, 'Float32 = ' + val);
 }
 
 // Разбор float64 18 (24)
 function parseFloat64(element){
 
-	var val = 'hz'; //parseInt(bytes[index + 1] + bytes[index + 2], 16);
+	var val = 'hz'; //parseInt(data_bytes[index + 1] + data_bytes[index + 2], 16);
 	addLog(element, 1, 8, 'Float64 = ' + val);
 }
 
 // Разбор date_time 19 (25)
 function parseDateTime(element){
 
-	var val = 'hz'; //parseInt(bytes[index + 1] + bytes[index + 2], 16);
+	var val = 'hz'; //parseInt(data_bytes[index + 1] + data_bytes[index + 2], 16);
 	addLog(element, 1, 12, 'DateTime = ' + val);
 }
 
 // Разбор date 1A (26)
 function parseDate(element){
 
-	var val = 'hz'; //parseInt(bytes[index + 1] + bytes[index + 2], 16);
+	var val = 'hz'; //parseInt(data_bytes[index + 1] + data_bytes[index + 2], 16);
 	addLog(element, 1, 5, 'Date = ' + val);
 }
 
 // Разбор time 1B (27)
 function parseTime(element){
 
-	var val = 'hz'; //parseInt(bytes[index + 1] + bytes[index + 2], 16);
+	var val = 'hz'; //parseInt(data_bytes[index + 1] + data_bytes[index + 2], 16);
 	addLog(element, 1, 4, 'Time = ' + val);
 }
