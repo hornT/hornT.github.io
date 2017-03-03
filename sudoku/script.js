29aa6f3b9af97a7be852b0522493162254370b4d
Neverov Nikolay <nikolay_neverov@adoriasoft.com>
2017-03-03 22:23:03 +0300

    small ref

--- sudoku/script.js
+++ sudoku/script.js
@@ -21,17 +21,21 @@ class SudokuCell{
     }
 
     Exclude(num){
+		const result = this.possibleValues.includes(num);
         this.possibleValues = this.possibleValues.filter(function(val){ return val !== num});
 
         if(this.possibleValues.length === 1){
             this.SetNum(this.possibleValues[0]);
         }
+
+		return result;
     }
 
     SetNum(num){
         this.inp.value = this.num = num;
         this.isSolve = true;
         this.inp.classList.add('solved-num');
+		console.debug('setnum. row: %s, column: %s, num: %s', this.row, this.column, num);
     }
 
     FillNotResolved(){
@@ -77,37 +81,44 @@ var Sudoku = (function() {
         ];
 
     function excludeNumbers(cell){
-        // Exclude by row
+		if(!cell.Num){
+			return false;
+		}
+
+		let result = false;
+        // Исключаем по строке
         for(let i = 0; i < 9; i++){
-            excludeNumberFromCell(mainArray[cell.Row][i], cell.Num);
+            result = excludeNumberFromCell(mainArray[cell.Row][i], cell.Num) || result;
         }
 
-        // Exclude by column
+        // Исключаем по колонке
         for(let i = 0; i < 9; i++){
-            excludeNumberFromCell(mainArray[i][cell.Column], cell.Num);
+            result = excludeNumberFromCell(mainArray[i][cell.Column], cell.Num) || result;
         }
 
-        // Exclude by block
+        // Исключаем внутри блока
         const row = Math.floor(cell.Row / 3) * 3;
         const column = Math.floor(cell.Column / 3) * 3;
         for(let i = 0; i < 3; i++){
             for(let j = 0; j < 3; j++){
-                excludeNumberFromCell(mainArray[row + i][column + j], cell.Num);
+                result = excludeNumberFromCell(mainArray[row + i][column + j], cell.Num) || result;
             }
         }
 
-        //setLastHeroes();
+		return result;
     }
 
     function excludeNumberFromCell(currCell, num){
         if(currCell.IsSolve === true){
-            return;
+            return false;
         }
         
-        currCell.Exclude(num);
+        const result = currCell.Exclude(num);
         if(currCell.IsSolve === true){
             excludeNumbers(currCell);
         }
+
+		return result;
     }
 
     function checkLastHeroUniqueByRow(value, row, column){
@@ -187,7 +198,7 @@ var Sudoku = (function() {
                     const valExists = checkLastHeroUniqueByRow(val, i, j) || checkLastHeroUniqueByColumn(val, i, j) || checkLastHeroUniqueByBlock(val, i, j);
                     if(valExists === true){
                         checkCell.SetNum(val);
-                        excludeNumbers(checkCell);
+						isSetValueExists = excludeNumbers(checkCell) || isSetValueExists;
                     }
                     return valExists;
                 });
@@ -199,11 +210,23 @@ var Sudoku = (function() {
         return isSetValueExists;
     }
 
-    // Solve sudoku
+	// Исключаем все повторения
+	function excludeAllNumbers(){
+		let res = false;
+		for(let i = 0; i < 9; i++){
+				for(let j = 0; j < 9; j++){
+					res = excludeNumbers(mainArray[i][j]) || res;
+				}
+			}
+
+		return res;
+	}
+
+    // Решаем
     function solve(){
 
         const solvedCells = [];
-        // Fill array
+        // Заполняем массив решений значениями пользователя
         for(let i = 0; i < 9; i++){
             for(let j = 0; j < 9; j++){
                 const cell = new SudokuCell(i, j);
@@ -213,18 +236,22 @@ var Sudoku = (function() {
                 }
             }
         }
-        //console.debug('solvedCells: %s', solvedCells.length);
-
-        solvedCells.forEach(function(element) {
-            excludeNumbers(element);
-        });
 
         let res = true;
         while(res === true){
-            res = setLastHeroes();
+			//res = false;
+			// Исключить повторения
+			res = excludeAllNumbers();
+			// for(let i = 0; i < 9; i++){
+				// for(let j = 0; j < 9; j++){
+					// res = excludeNumbers(mainArray[i][j]) || res;
+				// }
+			// }
+			// Установить "последнего героя"(остается единственный в строке, колонке, группе)
+            res = setLastHeroes() || res;
         }
 
-        // Fill not resolved
+        // Заполнить нерешенные ячейки для дальнейшего анализа
         for(let i = 0; i < 9; i++){
             for(let j = 0; j < 9; j++){
                 mainArray[i][j].FillNotResolved();
